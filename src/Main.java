bf6c970799a8d4d267dd3511fdde7e62e41dc51e
jkeating2025 <jkeating2025@cchsdons.com>
2024-01-11 08:18:23 -0800

    i commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     static Task[] toDoList = new Task[3];
@@ -15,19 +17,19 @@ public class Main {
 
     public static void main(String[] args) {
         //Add tasks
-        toDoList[0] = new Task("water my cat");
-        toDoList[0].setComplexity(3);
-        toDoList[0].setPriority(2);
-        toDoList[1] = new Task("cook my clothes");
-        toDoList[1].setComplexity(2);
-        toDoList[1].setPriority(2);
-        toDoList[2] = new Task("clean my soap");
-        toDoList[2].setComplexity(10);
-        toDoList[2].setPriority(1);
-        //Sort
-        //if(toDoList[0].getPriority < todoList.[1].getPriority && toDoList[0]
+        ArrayList<Task> toDoList = new ArrayList<Task>();
+        toDoList.add(new Task("water my cat"));
+        toDoList.get(0).setPriority(0);
+        toDoList.get(0).setComplexity(0);
+        toDoList.add(new Task("cook my clothes"));
+        toDoList.get(1).setPriority(1);
+        toDoList.get(1).setComplexity(1);
+        toDoList.add(new Task("clean my soap"));
+        toDoList.get(2).setPriority(2);
+        toDoList.get(2).setComplexity(2);
+
         //Reorder method
-        Arrays.sort(toDoList);
+        Collections.sort(toDoList);
         //Print list ordered
         System.out.println("Your To-Do list, in order of priority then complexity:");
         for (Task task : toDoList)
@@ -45,12 +47,13 @@ interface Complexity {
 class Task implements Priority, Complexity, Comparable<Task>{
     private String name;
     int priority;
+    int complexity;
     public String getName() {
         return name;
     }
     @Override
-    public void setPriority(int inputPriority) {
-        priority = inputPriority;
+    public void setPriority(int priority) {
+        this.priority = priority;
     }
     @Override
     public int getPriority() {
@@ -58,21 +61,30 @@ class Task implements Priority, Complexity, Comparable<Task>{
     }
     @Override
     public void setComplexity(int complexity) {
-
+        this.complexity = complexity;
     }
     @Override
     public int getComplexity() {
-        return 0;
+        return (complexity);
     }
     @Override
     public int compareTo(Task o) {
+        //Compare priority
         if(this.getPriority() > o.getPriority()){
             return 1;
         } else if (this.getPriority() < o.getPriority()) {
             return -1;
         }
+        //Priority is equal, therefore compare via complexity
         else{
-            return 0;
+            if(this.getComplexity() > o.getComplexity()){
+                return 1;
+            } else if (this.getComplexity() < o.getComplexity()) {
+                return -1;
+            }
+            else{
+                return 0;
+            }
         }
     }
     public Task (String name){
